ee33433e4ebde6806da603f889513b0b7fa42a75
Christoph Klamroth <christoph.klamroth@gmail.com>
2024-12-03 16:10:54 +0100

    Rename day directories

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import { day1 } from './day-01';
-import { day2 } from './day-02';
-import { day3 } from './day-03';
+import { day1 } from './day01';
+import { day2 } from './day02';
+import { day3 } from './day03';
 
 async function main(): Promise<void> {
   await day1();
